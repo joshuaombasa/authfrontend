dd68704a20d69e52ede5ad5b7de64b860dd3925b
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-28 15:56:56 +0300

    added routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
 import './App.css'
 
 import SignUp from './pages/Signup'
-
+import HeaderLayout from './components/HeaderLayout'
+import Login from './pages/Login'
 function App() {
   
 
   return (
     <>
-     <SignUp/>
+    <Routes>
+      <Route path='/' element={<HeaderLayout/>}>
+         <Route path='login' element={<Login/>}/>
+         <Route path='signup' element={<SignUp/>}/>
+      </Route>
+    </Routes>
     </>
   )
 }
