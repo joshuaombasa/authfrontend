d099a6566543e9934b9e7a511aaefb0bca4f1486
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-28 16:02:54 +0300

    added header  component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+    return (
+        <div className="header--section">
+            <a href="" className="logo">AUTHY</a>
+            <div className="c">
+                <Link to='login' className="login--link">Login</Link>
+                <Link to='signup' className="singup--link">Sign up</Link>
+            </div>
+        </div>
+    )
+}
