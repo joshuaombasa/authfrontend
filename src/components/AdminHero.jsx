0eaf4bd28e44705ea4f11f062c1051a15032ed4c
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-30 16:37:30 +0300

    added adminhero component

---
+++ src/components/AdminHero.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function AdminHero() {
+    return (
+        <div className="admin--container">
+            <h1>Welcome admin:</h1>
+        </div>
+    )
+}
